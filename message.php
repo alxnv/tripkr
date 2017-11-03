f94660234536abdd37b833d6db757bfecbc22dbf
Сергей <@31.129.128.248>
2017-11-03 14:48:55 +0300

    mobile2223

--- message.php
+++ message.php
@@ -6,6 +6,8 @@ if (!isset($conf35)) die;
 include_once "tools/my4.php";
 require_once "tools/funct.php";
 $title='US Korea. Сообщение';
+$my3->ismobile=(intval($_SESSION['scrwidth'])<768); // просмотр с мобильного устройства
+$my3->onecolumn=$my3->ismobile; // выводим все на экран в одну колонку
 
 ?>
 <? include "ssi-top.php"; ?>
