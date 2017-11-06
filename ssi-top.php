79b0b35c9accb9d7f31ec2463c4dc31b97f5c932
Сергей <@31.129.128.248>
2017-11-06 15:49:54 +0300

    404 23456

--- ssi-top.php
+++ ssi-top.php
@@ -15,7 +15,7 @@ if ($choosemobile) {
 
 
 $s10=$_SERVER["HTTP_USER_AGENT"];
-if (false && (strpos($s10,'+http://www.google.com/bot.html')!==false ||
+if ((strpos($s10,'+http://www.google.com/bot.html')!==false ||
         strpos($s10,'+http://yandex.com/bots')!==false)) {
     // google bot или yandex bot
     $_SESSION['scrwidth']=1280;
