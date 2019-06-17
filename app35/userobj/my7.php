c0049db342040df535895f23a4ecfdb18e26abcc
Саша <@192.168.0.239>
2019-06-17 14:57:58 +0300

    вернул фильтр zayav, добавил его обработку правильную 2

--- app35/userobj/my7.php
+++ app35/userobj/my7.php
@@ -62,6 +62,7 @@ class my7 {
             // символ (кавычку)
         return $s2;
      } else {
+         $db=$my7->db(); // получаем доступ к бд?
          return mysql_real_escape_string($s);
      }
  }
