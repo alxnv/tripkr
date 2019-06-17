dd1f58353d8c4f55bc92b9eff9480e0e0c042628
Саша <@192.168.0.239>
2019-06-17 15:07:11 +0300

    вернул фильтр zayav, добавил его обработку правильную 2

--- app35/userobj/my7.php
+++ app35/userobj/my7.php
@@ -62,8 +62,9 @@ class my7 {
             // символ (кавычку)
         return $s2;
      } else {
-         $db=my7::db(); // получаем доступ к бд?
-         return mysql_real_escape_string($s);
+         //$db=my7::db(); // получаем доступ к бд?
+         //return mysql_real_escape_string($s);
+         return PDO::quote ($s);
      }
  }
  /**
